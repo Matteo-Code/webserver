22ab27cf8bc35da372afad6d61d0828d166e40f7
Matteo-Code <matmo1418@gmail.com>
2025-04-23 19:11:32 +0200

    Simple HTTP Server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::Write, net::{TcpListener, TcpStream}};
+
+fn handle_client(mut stream: TcpStream) {
+    let body = "Hello, world!";
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/plain\r\n\r\n{}",
+        body.len(),
+        body
+    );
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
